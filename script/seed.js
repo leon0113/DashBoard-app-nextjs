3252b8d04e464f8d1bb38f788a97279c39e435a3
Leon <tahjib.leon399@gmail.com>
2024-03-01 00:07:29 +0600

    inserted data to sql table

--- script/seed.js
+++ script/seed.js
@@ -1,5 +1,7 @@
 const { db } = require("@vercel/postgres")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const { users } = require("../app/lib/placeholder-data");
+
 
 //seed users
 async function seedUsers(client) {
@@ -10,18 +12,40 @@ async function seedUsers(client) {
            CREATE TABLE IF NOT EXISTS users (
             id UUID DEFAULT uuid_generate_V4() PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
-            name VARCHAR(255) NOT NULL
-           )
-           `
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+           );
+           `;
+        console.log("created users table");
+
+        // insert data on user table 
+        const insertedUsers = await Promise.all(
+            users.map(async (user) => {
+                const hashedPassword = await bcrypt.hash(user.password, 10);
+                return client.sql`
+                INSERT INTO users(id, name, email, password)
+                VALUES(${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+                ON CONFLICT (id) DO NOTHING;
+                `;
+            })
+        );
+        console.log(`Seeded ${insertedUsers.length} users`);
+
+        return {
+            createTable,
+            users: insertedUsers
+        }
 
     } catch (error) {
         console.log("Failed to seed users", error);
     }
 }
 
+
+
 async function main() {
     const client = await db.connect();
-
+    await seedUsers(client)
 
     await client.end();
 }
